9a580f08c40668c37351d180f1d9a979a5a521f7
damien <contact.damiendiaz@gmail.com>
2022-12-01 20:31:53 +0100

    quest 9

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+
+        $program = new Program();
+        $program->setTitle('Super Mario');
+        $program->setSynopsis("A man who sold the world");
+        $program->setPoster('hello');
+        $program->setCategory($this->getReference('category_Action'));
+        $manager->persist($program);
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            CategoryFixtures::class,
+
+        ];
+
+    }
+}
